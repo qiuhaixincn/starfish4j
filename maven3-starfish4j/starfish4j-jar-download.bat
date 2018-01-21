dcb977b4f85694014e6af677ade97308ab987c10
qiuhx <qiuhaixincn@163.com>
2018-01-21 15:09:24 +0800

    初始化

--- maven3-starfish4j/starfish4j-jar-download.bat
+++ maven3-starfish4j/starfish4j-jar-download.bat
@@ -1,7 +1,5 @@
-rem  更新本地仓库
 rem  call mvn  clean package install -f starfish4j-pom.xml
 
-rem 自动下载jar包
 call mvn -f starfish4j-pom.xml dependency:copy-dependencies  -DoutputDirectory=./lib
 
 @pause
